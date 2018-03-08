c6937d5abfab612b6f3b5bd90785671c18157554
tdicksteinpomona <35976043+tdicksteinpomona@users.noreply.github.com>
2018-03-08 14:11:43 -0800

    Create problems.rb
    Beautifully horrible code

---
+++ problems.rb
@@ -0,0 +1,13 @@
+# TODO: fix this whole function
+def printHiabunchoftimesyay(numberofpotatoess)
+  $i = 0
+  $num = numberofpotatoess
+  while $i < $num  do
+   puts("hi" )
+   $i +=1
+   $p = 0
+   while $p < 3  do
+     puts("hi" )
+     $p +=1
+   end
+  end
